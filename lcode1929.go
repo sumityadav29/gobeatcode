01eb911eaac9a52e02bdc6d1f930f9840cb55a61
Sumit Yadav <sumit2911997@gmail.com>
2024-11-03 18:07:00 +0530

    solve leet code 1929

---
+++ lcode1929.go
@@ -0,0 +1,16 @@
+package main
+
+func getConcatenation(nums []int) []int {
+	length := 2 * len(nums)
+	concatenatedSlice := make([]int, length)
+
+	for i := 0; i < length; i++ {
+		if i < len(nums) {
+			concatenatedSlice[i] = nums[i]
+		} else {
+			concatenatedSlice[i] = nums[i-len(nums)]
+		}
+	}
+
+	return concatenatedSlice
+}
