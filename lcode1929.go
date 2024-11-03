4f9b12ee31338f15caa2eeb90eee21dd2d7c18a7
Sumit Yadav <sumit2911997@gmail.com>
2024-11-03 18:13:00 +0530

    use correct size in slice

--- lcode1929.go
+++ lcode1929.go
@@ -2,7 +2,7 @@ package main
 
 func getConcatenation(nums []int) []int {
 	length := 2 * len(nums)
-	concatenatedSlice := make([]int, length)
+	concatenatedSlice := make([]int, length, length)
 
 	for i := 0; i < length; i++ {
 		if i < len(nums) {
